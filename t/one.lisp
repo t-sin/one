e2e10510f8be8c81fbd533349e309423949de1de
t-sin <shinichi.tanaka45@gmail.com>
2015-09-08 18:50:38 +0900

    Basic tests more

--- t/one.lisp
+++ t/one.lisp
@@ -7,14 +7,78 @@
 
 ;; NOTE: To run this test file, execute `(asdf:test-system :one)' in your Lisp.
 
-(plan 4)
+(plan 5)
 
 (defun testdat (path)
   (merge-pathnames path *load-pathname*))
 
-(subtest "for --- expansion")
-(subtest "forl --- expansion")
+(subtest "for --- expansion"
+   (is-expand '(one:for (line #P"path"))
+              '(with-open-file (in #P"path"
+                                :direction :input
+                                :element-type 'character)
+                 (loop for line = (read in nil :eof)
+                    until (eq line :eof)
+                    collect line)))
 
+   (is-expand '(one:for (line one:stdin))
+              '(loop for line = (read cl:*standard-input* nil :eof)
+                  until (eq line :eof)
+                  collect line))
+
+   (is-expand '(one:for (line #P"path")
+                 (format t "~a~%" line))
+              '(with-open-file (in #P"path"
+                                :direction :input
+                                :element-type 'character)
+                 (loop for line = (read in nil :eof)
+                    until (eq line :eof)
+                    do (progn (format t "~a~%" line)))))
+
+   (is-expand '(one:for (line #P"path")
+                 (format t "~a~%" line)
+                 (format t "~a2~%" line))
+              '(with-open-file (in #P"path"
+                                :direction :input
+                                :element-type 'character)
+                 (loop for line = (read in nil :eof)
+                    until (eq line :eof)
+                    do (progn (format t "~a~%" line)
+                              (format t "~a2~%" line))))))
+
+(subtest "forl --- expansion"
+   (is-expand '(one:forl (line #P"path"))
+              '(with-open-file (in #P"path"
+                                :direction :input
+                                :element-type 'character)
+                 (loop for line = (read-line in nil :eof)
+                    until (eq line :eof)
+                    collect line)))
+
+   (is-expand '(one:forl (line one:stdin))
+              '(loop for line = (read-line cl:*standard-input* nil :eof)
+                  until (eq line :eof)
+                  collect line))
+
+   (is-expand '(one:forl (line #P"path")
+                 (format t "~a~%" line))
+              '(with-open-file (in #P"path"
+                                :direction :input
+                                :element-type 'character)
+                 (loop for line = (read-line in nil :eof)
+                    until (eq line :eof)
+                    do (progn (format t "~a~%" line)))))
+
+   (is-expand '(one:forl (line #P"path")
+                 (format t "~a~%" line)
+                 (format t "~a2~%" line))
+              '(with-open-file (in #P"path"
+                                :direction :input
+                                :element-type 'character)
+                 (loop for line = (read-line in nil :eof)
+                    until (eq line :eof)
+                    do (progn (format t "~a~%" line)
+                              (format t "~a2~%" line))))))
 
 
 (subtest "for (using read)"
@@ -51,7 +115,10 @@
           "lazy dog")))
 
   (subtest "with body"
-    (let ((s ""))
+    (let ((s "1
+3
+5
+7"))
       (is (one:for (l (testdat "strs.txt"))
             (setf s (format nil "~a ~a" s l)))
           nil)
@@ -61,5 +128,19 @@
             (setf s (format nil "~a ~a" s l)))
           nil)
       (is s " the quick brown fox jumps over the red lazy dog"))))
+
+(defmacro with-stdin ((str) &body body)
+  (let ((in (gensym)))
+    `(with-input-from-string (,in ,str)
+       (let ((*standard-input* ,in))
+         ,@body))))
+
+(subtest "interacting to *standard-input*"
+  (with-stdin ("1 2 3")
+    (is (one:for (l +stdin+))
+        '(1 2 3))
+    (is (one:forl (l +stdin+))
+        '("1 2 3"))))
+
 
 (finalize)
