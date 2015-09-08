23116d74f91a3a25766986085466e5aeb23f8752
t-sin <shinichi.tanaka45@gmail.com>
2015-09-09 00:07:57 +0900

    Experimental 'for' macro

--- one.lisp
+++ one.lisp
@@ -1,6 +1,38 @@
 (in-package :cl-user)
 (defpackage one
-  (:use :cl))
+  (:use :cl)
+  (:export :stdin
+           :for
+           :forl))
 (in-package :one)
 
-;; blah blah blah.
+
+(defparameter stdin *standard-input*)
+
+(defun body-form (var body)
+  (if body
+      `(do (progn ,@body))
+      `(collect ,var)))
+
+(defun loop-form (var input-stream read-fn body)
+  `(loop for ,var = (,read-fn ,input-stream nil :eof)
+         until (eq ,var :eof)
+         ,@(body-form var body)))
+
+(defmacro with-file-input ((var in) &body body)
+  (let ((fin (gensym)))
+    `(with-open-file (,fin ,in
+                      :direction :input
+                      :element-type 'character)
+       ,@body)))
+
+(defmacro for ((var in) &body body)
+  (cond ((or (typep in 'string)
+             (typep in 'pathname))
+         (let ((fin (gensym)))
+           `(with-open-file (,fin ,in
+                             :direction :input
+                             :element-type 'character)
+              ,(loop-form var fin 'read body))))
+        ((eq in 'stdin)
+         (loop-form var stdin 'read body))))
