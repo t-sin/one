f115d900906416231a41e161c5ef3cb96d16ec71
t-sin <shinichi.tanaka45@gmail.com>
2017-04-29 17:59:21 +0900

    Experimental; operation chaining

--- one.lisp
+++ one.lisp
@@ -1,41 +1,31 @@
 (in-package :cl-user)
 (defpackage one
   (:nicknames :o)
-  (:use :cl)
-  (:export :for
-           :forl))
+  (:use :cl))
 (in-package :one)
 
+(defun id (chained-op)
+  (lambda (input)
+    (funcall chained-op input)))
 
-(declaim (inline read-for))
-(defun read-for (input-stream read-fn body-fn)
-  (let ((eof (gensym)))
-    (loop for line = (funcall read-fn input-stream nil eof)
-       until (eq line eof)
-       collect (funcall body-fn line))))
+(defun $print (chained-op)
+  (lambda (input)
+    (print (funcall chained-op input))))
 
-(defmacro with-input-from-file ((var path) &body body)
-  `(with-open-file (,var ,path
-                    :direction :input
-                    :element-type 'character) ; use :inquisitor
-     ,@body))
+(defun /line (next-op)
+  (lambda (input)
+    (loop
+       :for line := (read-line input nil nil)
+       :while line
+       :do (funcall next-op line))))
 
-(defun call-read-for (in read-fn &optional (body-fn #'identity))
-  (cond ((or (typep in 'string)
-             (typep in 'pathname))
-         (with-input-from-file (fin in)
-           (read-for fin read-fn body-fn)))
-        ((eq in :stdin)
-         (read-for *standard-input* read-fn body-fn))))
-
-(defmacro deffor (name read-fn)
-  (let ((line (gensym)))
-    `(defmacro ,name ((var in) &body body)
-       (let ((,line (gensym)))
-         (if body
-             `(call-read-for ,in ,,read-fn
-                             (lambda (,,line) (let ((,var ,,line)) ,@body)))
-             `(call-read-for ,in ,,read-fn))))))
-
-(deffor for #'read)
-(deffor forl #'read-line)
+#|
+CL-USER>  (chain 3 #'id #'$print #'identity)
+;=> 3
+3
+|#
+(defun chain (input &rest operators)
+  (loop
+     :for op :in operators
+     :for chained-op := op :then (funcall chained-op op)
+     :finally (return-from chain (funcall chained-op input))))
