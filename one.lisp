4edbe2e1dec9aa4a8bec72e48faf6049b9badcee
t-sin <shinichi.tanaka45@gmail.com>
2017-05-02 23:51:50 +0900

    Now, operators are renamed as chain operators

--- one.lisp
+++ one.lisp
@@ -5,37 +5,40 @@
 (in-package :one)
 
 #|
-CL-USER>  (chain 3 #'id #'$print #'identity)
+CL-USER>  (chain 3 #'id #'$print)
 ;=> 3
 3
 |#
-(defun chain (input &rest operators)
+(defun chain (input &rest chain-operators)
   (loop
-     :for (op . rest) :on (reverse operators)
+     :for (op . rest) :on (reverse chain-operators)
      :for chained-op := (funcall op #'identity) :then (funcall op chained-op)
      :finally (return-from chain (funcall chained-op input))))
 
-(defmacro define-op (fn-name (input-var) &body body)
+(defmacro define-chain-op (fn-name (input-var) &body body)
   (let ((chained-op (gensym)))
     `(defun ,fn-name (,chained-op)
        (lambda (,input-var)
          (funcall ,chained-op (progn ,@body))))))
 
+;;; chain-operators
 ;;; they are constructive operators, not core.
 ;;; they will be moved when finished experimental
-(define-op id (obj)
+(define-chain-op id (obj)
   obj)
 
-(define-op add1 (num)
+(define-chain-op add1 (num)
   (1+ num))
 
-(define-op reads (str)
+(define-chain-op reads (str)
   (read-from-string str))
 
-(define-op $print (obj)
+(define-chain-op $print (obj)
   (print obj))
 
-;; cannot define with define-op
+
+;;; per or split operators?
+;;; cannot define with define-op
 #|
 CL-USER> (with-input-from-string (in (format nil "42~%43~%44"))
            (chain in #'/line #'reads #'add1))
