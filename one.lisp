999a3633333984d4624ca368b600194d926e2018
t-sin <shinichi.tanaka45@gmail.com>
2017-08-02 13:09:55 +0900

    Clean up

--- one.lisp
+++ one.lisp
@@ -9,42 +9,8 @@
            :for))
 (in-package :one)
 
-;;;; this is a rough sketch for `one`
-
-#|
-- can use standard Common Lisp operators/functions
-- can use other library functions (e.g. split-sequence)
-- value-to-value operations are lazy evaluated
-    - distinguish a kind of *one operators* by '<', '>'
-
-(one:for "1,2,3,4,5" (<split #\,) read (+ 1) (>remove-if #'oddp) >+)
-=> (let* ((input "1,2,3,4,5")
-          (pipe1 (make-pipe-from-sequense (split-sequence #\, input)))
-          (pipe2 (transform-pipe pipe1 (lambda (x) (+ 1 (lambda (x) (read x))))))
-          (pipe3 (make-pipe pipe2 (remove-if #'oddp {})))
-          (pipe4 (make-pipe pipe3 (+ @{}))))
-      (dump-pipe pipe4))
-; => 8
-|#
-
-#|
-(one:for <input> <transform...>)
-transforms:
-- symbol ... 1 args function
-- list ... muiti-args function (but passed one args: input)
-  - simple list ... args placed at last
-  - list has _ symbol ... args placed at position of _
-- readers ... function has special behavior; result stores in object pipe
-  - $<(fun args... _) ... multi time firing of successor
-  - $>(fun args... _) ... outputs maybe fewer than input
-
-(one:for1 <input> <transform>)
-|#
-
-
-;;;; one: elementary functions
-
 ;;; input
+
 (defun read* (stream)
   (read stream nil :eof))
 
@@ -84,26 +50,16 @@ transforms:
               :do (funcall op e))))))
 
 ;;; processing
-;; ex)
-;; - filter
-;;
-;; reader macro:
-;;   $?(fn args...)
-;;   $?fn
+
 (defun $call-if (predicate next-op)
   (lambda (input)
     (when (funcall predicate input)
       (funcall next-op input))))
 
-;; ex)
-;; - split
 (defun $map (next-op)
   (lambda (sequence)
     (mapcar next-op (coerce sequence 'list))))
 
-;; ex)
-;; - sort
-;; - as list
 (defun $gather (gather-op)
   (let (buffer)
     (flet ((slurp (input) (push input buffer))
@@ -111,8 +67,8 @@ transforms:
              (funcall op (funcall gather-op (nreverse buffer)))))
       (values #'slurp #'barf))))
 
-
 ;;; DSL
+
 (defun connective-p (e)
   (member e '(< > $ ?)
           :test (lambda (a b)
