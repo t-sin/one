415c7d6b221f8d860d40215e85333146b2c00bbc
t-sin <shinichi.tanaka45@gmail.com>
2017-08-02 17:57:23 +0900

    If input is spacified as '-', it is standard input

--- one.lisp
+++ one.lisp
@@ -142,4 +142,6 @@
 ;; (for #P"hoge.log" < read-line ? (search "fuga" _) > (sort _ <) $ print)
 ;; == $ cat hoge.log | grep "fuga" | sort
 (defmacro for (input &body body)
-  `(funcall ,(build (parse (replace-connective body))) ,input))
+  (if (string= (symbol-name input) "-")
+      `(funcall ,(build (parse (replace-connective body))) ,*standard-input*)
+      `(funcall ,(build (parse (replace-connective body))) ,input)))
