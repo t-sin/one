7b23e45616c28982ada235b9a0921082165b0906
t-sin <shinichi.tanaka45@gmail.com>
2017-08-18 14:53:54 +0900

    WIP: tests for `$scan`

--- tests/core.lisp
+++ tests/core.lisp
@@ -4,7 +4,28 @@
 (in-package :one/tests/core)
 
 (deftest internal-operator-scan-test
-  (diag "ok"))
+  (testing "stream"
+    (testing "function which is returned by `$scan`"
+      (ok (typep (one::$scan (make-string-input-stream "hogehoge") #'one:read-line*)
+                 'function)))
+
+    (testing "arity of returned function is 1"
+      (ng (signals
+           (funcall (one::$scan (make-string-input-stream "hogehoge") #'one:read-line*))
+           'simple-error))
+      (ok (null (funcall (one::$scan (make-string-input-stream "hogehoge") #'one:read-line*)
+                         #'identity)))
+      (ng (signals
+           (funcall (one::$scan (make-string-input-stream "hogehoge") #'one:read-line*)
+                    #'identity 42)
+           'simple-error)))
+
+    (testing "read-fn which specified is used")
+    (testing "read-fn must be returns :eof when stream end")
+    (testing "op is called for all stream elements"))
+
+  (testing "pathname")
+  (testing "sequence"))
 
 (deftest internal-operator-call-if-test
   (diag "ok"))
