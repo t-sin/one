b558210f324a36b433078dd573c3db3a900de6b5
t-sin <shinichi.tanaka45@gmail.com>
2017-08-18 14:18:44 +0900

    Adds test skeltons

--- tests/core.lisp
+++ tests/core.lisp
@@ -3,3 +3,14 @@
   (:use :cl :rove))
 (in-package :one/tests/core)
 
+(deftest internal-operator-scan-test
+  (diag "ok"))
+
+(deftest internal-operator-call-if-test
+  (diag "ok"))
+
+(deftest internal-operator-gather-test
+  (diag "ok"))
+
+(deftest internal-operator-fold-test
+  (diag "ok"))
